27e477ca38a47f2938168378411d60aade036862
Rendi Rizki <rendirizkir@gmail.com>
2021-09-29 23:59:16 +0700

    initial commit

---
+++ src/components/ThemeToggleButton.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+
+export const ThemeToggleButton = () => {
+  const [theme, setTheme] = useState('light');
+  const colorTheme = theme === 'light' ? 'dark' : 'light';
+
+  useEffect(
+    () => {
+      const root = window.document.documentElement;
+      root.classList.remove(colorTheme);
+      root.classList.add(theme);
+    },
+    [theme],
+    colorTheme
+  );
+
+  return (
+    <div>
+      <span onClick={() => setTheme(colorTheme)}>
+        {colorTheme === 'light' ? (
+          <svg
+            className="h-6 w-6 text-gray-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+            />
+          </svg>
+        ) : (
+          <svg
+            className="h-6 w-6 text-gray-400"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+            />
+          </svg>
+        )}
+      </span>
+    </div>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        h1: { fontSize: theme('fontSize.2xl') },
+        h2: { fontSize: theme('fontSize.xl') },
+        h3: { fontSize: theme('fontSize.lg') },
+      });
+    }),
+  ],
+};
